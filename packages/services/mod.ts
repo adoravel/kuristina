56000d1de8b2b78dc6f0a9cbfb165a27e46ec917
kyu.re <k@kyu.re>
2026-07-30 17:04:59 -0300

    build(services): add package exports for all services

---
+++ packages/services/mod.ts
@@ -0,0 +1,8 @@
+/**
+ * kuristina, a ~~kitchen~~ bathroom sink discord bot
+ * Copyright (c) 2025-2026 kyu.re
+ * SPDX-License-Identifier: AGPL-2.0-or-later
+ */
+
+export * from "./translation/mod.ts";
+export * from "./google/batchexecute.ts";
