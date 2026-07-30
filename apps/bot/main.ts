2edc5f2021bd4ccf8f59c4c50cd7a84d22e7f038
kyu.re <k@kyu.re>
2026-07-30 17:05:30 -0300

    feat(app): add bot entrypoint with signal handling

---
+++ apps/bot/main.ts
@@ -0,0 +1,25 @@
+/**
+ * kuristina, a ~~kitchen~~ bathroom sink discord bot
+ * Copyright (c) 2025-2026 kyu.re
+ * SPDX-License-Identifier: AGPL-2.0-or-later
+ */
+
+import { initialise, shutdown } from "@kuristina/discord-bot";
+import { initialiseClient } from "@kuristina/discord-client";
+
+if (import.meta.main) {
+	const res = await initialise();
+	if (!res.ok) throw res.error;
+
+	const client = initialiseClient();
+	if (client) await client.start();
+
+	for (const signal of ["SIGINT", "SIGTERM"] as const) {
+		Deno.addSignalListener(signal, async () => {
+			console.log(`\nreceived ${signal}, shutting down...`);
+			await shutdown();
+			await client?.shutdown();
+			Deno.exit(0);
+		});
+	}
+}
