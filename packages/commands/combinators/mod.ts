67d43a7bbac703d9989dc7618493ee26f0931679
kyu.re <k@kyu.re>
2026-07-30 17:02:37 -0300

    feat(commands): parser combinators with backtracking

---
+++ packages/commands/combinators/mod.ts
@@ -0,0 +1,11 @@
+/**
+ * kuristina, a ~~kitchen~~ bathroom sink discord bot
+ * Copyright (c) 2025-2026 kyu.re
+ * SPDX-License-Identifier: AGPL-2.0-or-later
+ */
+
+export * from "./constructions.ts";
+export * from "./error.ts";
+export * from "./parse.ts";
+export * from "./primitives.ts";
+export * from "./stream.ts";
