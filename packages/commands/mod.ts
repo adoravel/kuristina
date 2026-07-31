b7aca32dd3b87ebde33b7a91969421643cc7ffe1
kyu.re <k@kyu.re>
2026-07-31 18:15:52 -0300

    fix(commands): correct import paths for nested command files

--- packages/commands/mod.ts
+++ packages/commands/mod.ts
@@ -6,28 +6,27 @@
 
 export * from "./lib/mod.ts";
 
-const cmd = (_: TemplateStringsArray, ...values: string[]) =>
-	import(`./src/${values[0]}.tsx`).then((m) => m.default);
+const cmd = (path: string) => import(`./src/${path}.tsx`).then((m) => m.default);
 
 /** meow */
 export const commands = {
 	get help() {
-		return cmd`help`;
+		return cmd("help");
 	},
 	get ping() {
-		return cmd`ping`;
+		return cmd("ping");
 	},
 	get role() {
-		return cmd`role`;
+		return cmd("role");
 	},
 	get translate() {
-		return cmd`translate`;
+		return cmd("translate");
 	},
 	get whoknows() {
-		return cmd`whoknows`;
+		return cmd("fm/whoknows");
 	},
 	get lastfm() {
-		return cmd`login`;
+		return cmd("fm/login");
 	},
 	get all() {
 		return [this.help, this.ping, this.role, this.translate, this.whoknows, this.lastfm];
